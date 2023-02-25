11889801160fb93c657c41f57f6bb058b5087333
Silvio Ruiz <silviojrck@gmail.com>
2023-02-24 22:01:44 -0300

    agregando la logica al ejercicio05

---
+++ js/ejercicio05.js
@@ -0,0 +1,5 @@
+function ejercicio05(){
+    const numero1 = prompt("Ingrese un numero");
+    const numero2 = prompt("Ingrese otro numero");
+    document.write("La suma de los numeros ingresados es: ", parseInt(numero1) + parseInt(numero2));
+}
