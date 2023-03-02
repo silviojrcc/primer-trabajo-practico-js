3b61824ff0d6c797c62d99b7062711273c974e4c
Silvio Ruiz <silviojrck@gmail.com>
2023-03-02 03:12:38 -0300

    Solucionando un problema con el if del ejercicio05

--- js/ejercicio05.js
+++ js/ejercicio05.js
@@ -2,7 +2,7 @@ function ejercicio05(){
     const numero1 = prompt("Ingrese un numero");
     const numero2 = prompt("Ingrese otro numero");
 
-    if (!numero1 === "" && !numero2 === "" && !isNaN(numero1) && !isNaN(numero2)) {
+    if (!(numero1 == "") && !(numero2 == "") && !isNaN(numero1) && !isNaN(numero2)) {
         const resultado = parseInt(numero1) + parseInt(numero2);
         document.write("<h1>La suma de los numeros ingresados es: " + resultado + "</h1>");
     } else {
