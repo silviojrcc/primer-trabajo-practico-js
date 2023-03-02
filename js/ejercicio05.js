269d8c929dd5671494d8b5e1cc8b5f4425bf9bdb
Silvio Ruiz <silviojrck@gmail.com>
2023-03-02 03:09:24 -0300

    Corrigiendo document.write de ejercicio5

--- js/ejercicio05.js
+++ js/ejercicio05.js
@@ -1,12 +1,10 @@
 function ejercicio05(){
     const numero1 = prompt("Ingrese un numero");
-    console.log(numero1);
     const numero2 = prompt("Ingrese otro numero");
 
-    console.log(numero2);
     if (!numero1 === "" && !numero2 === "" && !isNaN(numero1) && !isNaN(numero2)) {
         const resultado = parseInt(numero1) + parseInt(numero2);
-        document.write("La suma de los numeros ingresados es: ", resultado);
+        document.write("<h1>La suma de los numeros ingresados es: " + resultado + "</h1>");
     } else {
         alert("Por favor, ingrese números válidos");
     }
