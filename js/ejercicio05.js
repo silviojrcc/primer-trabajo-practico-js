55b55dedea61a5c4bbbd16569a95a9c7c63e5532
Silvio Ruiz <silviojrck@gmail.com>
2023-03-02 02:35:23 -0300

    agregando validaciones al ejercicio05

--- js/ejercicio05.js
+++ js/ejercicio05.js
@@ -1,5 +1,13 @@
 function ejercicio05(){
     const numero1 = prompt("Ingrese un numero");
+    console.log(numero1);
     const numero2 = prompt("Ingrese otro numero");
-    document.write("La suma de los numeros ingresados es: ", parseInt(numero1) + parseInt(numero2));
+
+    console.log(numero2);
+    if (numero1 === "" || numero2 === "" || isNaN(numero1) || isNaN(numero2)) {
+        alert("Por favor, ingrese números válidos");
+    } else {
+        const resultado = parseInt(numero1) + parseInt(numero2);
+        document.write("La suma de los numeros ingresados es: ", resultado);
+    }
 }
