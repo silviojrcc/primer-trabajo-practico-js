0058023ad7b3f9cdac09561c4bd3801403391639
Silvio Ruiz <silviojrck@gmail.com>
2023-03-02 03:07:44 -0300

    Corrigiendo document.write de ejercicio3

--- js/ejercicio04.js
+++ js/ejercicio04.js
@@ -2,7 +2,7 @@ function ejercicio04(){
     let nombreUsuario = prompt("Escribe tu nombre");
 
     if (!(nombreUsuario === "")) {
-        document.write("Hola ", nombreUsuario);
+        document.write("<h1>Hola " + nombreUsuario +"</h1>");
     } else {
         alert("Debes ingresar un nombre válido");
     }
